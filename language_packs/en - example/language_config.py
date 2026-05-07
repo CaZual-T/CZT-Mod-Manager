5ca372f35fd71f99a116420c104091709f41c616
CaZual_T <86449182+CaZual-T@users.noreply.github.com>
2026-05-07 00:40:47 -0400

    Delete language_packs/en - example directory

--- language_packs/en - example/language_config.py
+++
@@ -1,5 +0,0 @@
-# English language pack // Place language folder into CZT Mod Manager/plugins/language_packs/>folder here<
-
-LANGUAGE_NAME = "English - Example" # (native name of the language, e.g., 'Español', 'Deutsch', 'Français', 'Русский'). (This is what appears within the settings drop down menu)
-
-LANGUAGE_CODE = "en - example" # language code (e.g., 'es', 'de', 'fr', 'ru'). IMPORTANT: This should match the languages folder name, it can be anything just needs to be consistent with the folder name.
