8f559c882b270a2544d466ce77dcf01ba6fcfadf
CaZual_T <86449182+CaZual-T@users.noreply.github.com>
2026-05-14 11:47:29 -0400

    Delete language_packs/es directory

--- language_packs/es/language_config.py
+++
@@ -1,5 +0,0 @@
-# Paquete de idioma espanol // Coloca la carpeta de idioma en CZT Mod Manager/plugins/language_packs/>carpeta aqui<
-
-LANGUAGE_NAME = "Español - Traducido con IA" # (nombre nativo del idioma, p. ej., 'Espanol', 'Deutsch', 'Francais', 'Russkiy'). (Esto es lo que aparece en el menu desplegable de configuracion)
-
-LANGUAGE_CODE = "es" # codigo de idioma (p. ej., 'es', 'de', 'fr', 'ru'). IMPORTANTE: Debe coincidir con el nombre de la carpeta del idioma, puede ser cualquiera mientras sea consistente con el nombre de la carpeta.
